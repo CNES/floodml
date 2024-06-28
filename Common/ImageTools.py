6e5d5c30b4f6c34865d25e69affa9a963c2c9f20
Fatras Christophe <cfatras@groupcls.com>
2024-06-28 10:01:40 +0000

    Update ImageTools.py

--- Common/ImageTools.py
+++ Common/ImageTools.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Copyright (C) CNES, CLS, SIRS - All Rights Reserved
+Copyright (C) CNES, CLS - All Rights Reserved
 This file is subject to the terms and conditions defined in
 file 'LICENSE.md', which is part of this source code package.
 
 Project:        FloodML, CNES
 """
+
 
 
 from PIL.Image import BICUBIC
